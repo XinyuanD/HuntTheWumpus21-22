b9a3f2f611df09b5ad97a98cb4de3fd03db6cefb
sanjanas18 <94811483+sanjanas18@users.noreply.github.com>
2021-11-30 22:19:10 -0800

    Create wumpus.java

---
+++ HuntTheWumpus/src/wumpus.java
@@ -0,0 +1,15 @@
+
+//wumpus, we can use a lot of stuff from player here
+//we just need random spawn, which we can take from wumpus
+//and a static boolean isalive for our game play to see if its alive then continue or else if false then game over
+
+public class Wumpus {
+   
+  
+  public static boolean isAlive;
+
+  
+  public wumpus()
+  
+  
+}
