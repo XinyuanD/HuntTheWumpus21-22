61d30916a43714693e08ac552527f5116411bc39
ImJordanNetz <94811498+ImJordanNetz@users.noreply.github.com>
2021-11-30 19:18:04 -0800

    Update player.java
    The player can move from room to room

--- HuntTheWumpus/src/player.java
+++ HuntTheWumpus/src/player.java
@@ -2,29 +2,53 @@
 
 //what methods we want, etc.
 
-public class Player { 
+public class player { 
   
   //DATA
- int roomNum;
- int numArrows;
+ public room currentRoom;
+ public int numArrows;
  //int score 
   //we can either put score as a property of this object or make it something else entirely
   
   
   //created object
-  public Player(int roomNum, int numArrows) {
-    this.roomNum = roomNum;
+  public player(room room, int numArrows) {
+    this.currentRoom = room;
     this.numArrows = numArrows;
   }
   
-  
-  //classice getters. we can put the setters later. this will be important for updating the player's stage
-  //weumpus will have similar structure
-  public getRoom() {
-    return roomNum;
+  //moves player from room to room
+  public void moveAbove(){
+    currentRoom = Main.rooms[currentRoom.above-1];
+  }
+
+  public void moveBellow(){
+    currentRoom = Main.rooms[currentRoom.bellow-1];
+  }
+
+  public void moveTopLeft(){
+    currentRoom = Main.rooms[currentRoom.leftTop-1];
+  }
+
+  public void moveTopRight(){
+    currentRoom = Main.rooms[currentRoom.rightTop-1];
+  }
+
+  public void moveBottomLeft(){
+    currentRoom = Main.rooms[currentRoom.leftBot-1];
+  }
+
+  public void moveBottomRight(){
+    currentRoom = Main.rooms[currentRoom.rightBot-1];
+  }
+
+  //classic getters. we can put the setters later. this will be important for updating the player's stage
+  //wumpus will have similar structure
+  public room getRoom() {
+    return currentRoom;
   }
   
-  public getNumArrows() {
+  public int getNumArrows() {
     return numArrows;
   }
   
