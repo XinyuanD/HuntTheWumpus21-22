805f6a375fa200bb3db41db3565f6a03fcf26cfb
sanjanas18 <94811483+sanjanas18@users.noreply.github.com>
2021-11-30 19:01:52 -0800

    Update player.java

--- HuntTheWumpus/src/player.java
+++ HuntTheWumpus/src/player.java
@@ -4,9 +4,31 @@
 
 public class Player { 
   
+  //DATA
+ int roomNum;
+ int numArrows;
+ //int score 
+  //we can either put score as a property of this object or make it something else entirely
   
   
+  //created object
+  public Player(int roomNum, int numArrows) {
+    this.roomNum = roomNum;
+    this.numArrows = numArrows;
+  }
   
+  
+  //classice getters. we can put the setters later. this will be important for updating the player's stage
+  //weumpus will have similar structure
+  public getRoom() {
+    return roomNum;
+  }
+  
+  public getNumArrows() {
+    return numArrows;
+  }
+  
+  //next will be a spawn method to put in a random room
   
   
 }
