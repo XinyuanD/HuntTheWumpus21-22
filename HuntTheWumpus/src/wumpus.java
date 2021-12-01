5be7a7222539164fe4e396088ec3054f21ac88e4
sanjanas18 <94811483+sanjanas18@users.noreply.github.com>
2021-11-30 22:24:06 -0800

    Update wumpus.java

--- HuntTheWumpus/src/wumpus.java
+++ HuntTheWumpus/src/wumpus.java
@@ -2,14 +2,30 @@
 //wumpus, we can use a lot of stuff from player here
 //we just need random spawn, which we can take from wumpus
 //and a static boolean isalive for our game play to see if its alive then continue or else if false then game over
-
+import java.util.Random;
 public class Wumpus {
    
-  
+  //probably just a static boolean to reference in the game play or even in player
+   //we could have a script just for specific game play, our main, where it starts, launches, has player take move as long as isAlive == true
   public static boolean isAlive;
-
+   public room currentRoom; 
   
-  public wumpus()
+  //spawns
+  public Wumpus() {
+     Random rand = new Random();
+     //picks random room
+     this.currentRoom = Main.rooms[rand.nextInt(Main.rooms.length-1)];
+     //says its alive
+     isAlive = true; 
+   
+  }
+  
+   //sets room
+  public wumpus(room currentRoom) {
+     this.currentRoom = currentRoom;
+     
+  }
+   
   
   
 }
