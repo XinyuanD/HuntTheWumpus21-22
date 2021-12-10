40249b20e9e1e642ac59c48eaee9aed42ab346f2
ImJordanNetz <94811498+ImJordanNetz@users.noreply.github.com>
2021-12-09 22:32:36 -0800

    Update and rename roomTestMain.java to main.java

---
+++ HuntTheWumpus/src/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        game newGame = new game();
+        newGame.caveToString();
+        System.out.println(newGame.getRoomCount());
+    }
+}
