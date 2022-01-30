fe3d9d9c89ae830b2a6a9a4e43b7aa029c292615
ImJordanNetz <94811498+ImJordanNetz@users.noreply.github.com>
2022-01-29 19:17:05 -0800

    Update Wumpus.cs

--- Wumpus.cs
+++ Wumpus.cs
@@ -1,59 +1,70 @@
+using System;
 using System.Collections;
-
+//wumpus, we can use a lot of stuff from player here
+//we just need random spawn, which we can take from wumpus
+//and a static boolean isalive for our game play to see if its alive then continue or else if false then game over
 namespace Wumpus
 {
-    public class trivia
-    {
-        // private ArrayList rooms;
-        private ArrayList questions;
-        private ArrayList answers;
+public class wumpus {
+   
+  //probably just a static boolean to reference in the game play or even in player
+   //we could have a script just for specific game play, our main, where it starts, launches, has player take move as long as isAlive == true
+    private bool alive;
+    private room currentRoom;
+    private game game;
+  
+  //spawns
+  public wumpus(game game) {
+     Random rand = new Random();
+     while (this.currentRoom != game.getPlayer().getRoom()){
+      this.currentRoom = game.getCave()[rand.Next(game.getCave().Length)];
+     }
+     this.game = game;
+     //picks random room
+     //says its alive
+     alive = true; 
+   
+  }
+  
+   //sets room
+  public wumpus(room currentRoom, game game) {
+     this.currentRoom = currentRoom;
+     this.game = game;
+     
+  }
 
-        private game game;
-
-        public trivia(game game) {
-            this.game = game;
-            questions = new ArrayList();
-            answers = new ArrayList();
-            using (StreamReader file = new StreamReader("triviaQs.txt")) {  
-                int counter = 0;  
-                string line;  
-                while ((line = file.ReadLine()) != null) {  
-                    if (counter % 2 == 0) {
-                        questions.Add(line);
-                    } else {
-                        answers.Add(line);
-                    }
-                counter++;  
-                }  
-            } 
-        }
-
-        public bool askQuestions(int amount, int needCorrect) {
-            Random rand = new Random();
-            int right = 0;
-            int wrong = 0;
-            for (int i = 0; i < amount; i++) {
-                if (wrong > amount-needCorrect) {  
-                    Console.WriteLine("You lost the game!");
-                    return false;
-                } else {  
-                    int idx = rand.Next(questions.Count);
-                    string question = (string) questions[idx]; // random question
-                    string answer = (string) answers[idx]; // random answer
-                    Console.WriteLine(question);
-                    string userAnswer = Console.ReadLine();
-                    if (String.Equals(answer.ToLower(), userAnswer.ToLower())) { // check user answer
-                        right++;
-                    } else {
-                        wrong++;
-                    }
-                }
-            }   
-            if (right >=needCorrect) {
-                Console.WriteLine("You beat the trivia! You will now be respawned.");
-                return true;
-            }
-            return true;
-        }
+  public void move2to4RandomRoom(){
+    Random rand = new Random();
+    int moves = rand.Next(3) + 2;
+    bool[] visited = new bool[game.getRoomCount()];
+    visited[currentRoom.getVal()-1] = true;
+    int numVisited = 0;
+    while (numVisited <= moves){
+      int nextRoom = rand.Next(currentRoom.getConnected().Length);
+      if(!visited[currentRoom.getConnected()[nextRoom]]){
+        currentRoom = game.getCave()[nextRoom];
+        visited[nextRoom] = true;
+        numVisited++;
+      }
     }
+  }
+
+  public void moveRandomRoom(){
+    Random rand = new Random();
+    currentRoom = game.getCave()[currentRoom.getConnected()[rand.Next(currentRoom.getConnected().Length)]-1];
+  }
+
+  public bool isAlive(){
+      return alive;
+  }
+
+  public room getRoom(){
+      return currentRoom;
+  }
+
+  public game getGame(){
+    return game;
+  }
+  
+}
 }
